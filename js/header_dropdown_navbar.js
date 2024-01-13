706cba0e0a441d806c1f4c387cb4ea6cbd095177
GMKRAKEN23 <[messinagiuseppe9423@gmail.com]>
2024-01-13 21:00:48 +0100

    header_dropdown_navbar.js

---
+++ js/header_dropdown_navbar.js
@@ -0,0 +1,13 @@
+let dropNav = document.querySelector('.header_nav_bar_drodpdown_container');
+let linkMore = document.querySelector('#more_link');
+
+linkMore.addEventListener('click', function () {
+
+    if (dropNav.style.display === 'none' || dropNav.style.display === '') {
+        dropNav.style.display = "flex";
+        linkMore.style.color = "#1786F9";
+    } else {
+        dropNav.style.display = "none";
+        linkMore.style.color = "#fbfafa";
+    }
+})
